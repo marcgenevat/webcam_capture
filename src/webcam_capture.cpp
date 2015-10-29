acfda003c731fa36a961448a6dd49994fdb91be9
Marc Genevat <mgenevat@gmail.com>
2015-10-29 23:21:55 +0100

    Exercici_1_4_Sistemes_de_Percepció

--- src/webcam_capture.cpp
+++ src/webcam_capture.cpp
@@ -3,59 +3,95 @@
 #include <iostream>
 #include <cstdlib>
 
-int main(int argc, char *argv[]) 
-{
+int main (int argc, char *argv[]) {
+	
 	//OpenCV video capture object
-    cv::VideoCapture camera;
-	
+	cv::VideoCapture camera;
+
 	//OpenCV image object
-    cv::Mat image;
-	
-	//camera id . Associated to device number in /dev/videoX
-	int cam_id; 
-	
+	cv::Mat image;
+
+	//Camera id		Associated to a device number in /dev/videoX
+	int cam_id;
+
 	//check user args
-	switch(argc)
-	{
-		case 1: //no argument provided, so try /dev/video0
-			cam_id = 0;  
-			break; 
-		case 2: //an argument is provided. Get it and set cam_id
+	switch (argc) {
+		
+		case 1:		//No argument provided, so try /dev/video0
+			
+			cam_id = 0;
+			break;
+
+		case 2:		//An argument is provided. Get it and set cam_id
+
 			cam_id = atoi(argv[1]);
-			break; 
-		default: 
-			std::cout << "Invalid number of arguments. Call program as: webcam_capture [video_device_id]. " << std::endl; 
-			std::cout << "EXIT program." << std::endl; 
-			break; 
+			break;
+
+		default:
+			
+			std::cout<<"Invalid number of arguments. Call program as: wecam_capture [video_device_id]."<<std::endl;
+			std::cout<<"EXIT program."<<std::endl;
+			break;
+
 	}
-	
-	//advertising to the user 
-	std::cout << "Opening video device " << cam_id << std::endl;
 
-    //open the video stream and make sure it's opened
-    if( !camera.open(cam_id) ) 
-	{
-        std::cout << "Error opening the camera. May be invalid device id. EXIT program." << std::endl;
-        return -1;
-    }
+	//Advertising to the user
+	std::cout<<"Opening video device "<<cam_id<<"..."<<std::endl;
 
-    //capture loop. Out of user press a key
-    while(1)
-	{
+	//Open the video stream and make sure it's opened
+	if (!camera.open(cam_id)) {
+		
+		std::cout<<"Error opening the camera. May be an invalid device id. EXIT program."<<std::endl;
+		return -1;
+
+	}
+
+	//Capture loop. Out when user press a key
+	while (1) {
+		
 		//Read image and check it
-        if(!camera.read(image)) 
-		{
-            std::cout << "No frame" << std::endl;
-            cv::waitKey();
-        }
+		if (!camera.read(image)) {
+
+			std::cout<<"No frame"<<std::endl;
+			cv::waitKey();
+
+		}
+
+		int central_pixel_y = image.rows/2;
+		int central_pixel_x = image.cols/2;
+
+		//Get pixel 3 channel values
+		cv::Vec3b intensity = image.at<cv::Vec3b>(central_pixel_y, central_pixel_x);
+		int blue = intensity.val[0];
+		int green = intensity.val[1];
+		int red = intensity.val[2];
+
+		//Print image dimensions
+		std::cout<<"Image size is: "<<image.rows<<"x"<<image.cols<<std::endl;
+
+		//Print central pixel coordinates
+		std::cout<<"The central pixel is at: "<<central_pixel_y<<"x"<<central_pixel_x<<std::endl;
+
+		//Print central pixel 3 original channel values
+		std::cout<<"And its original 3 channel values are: "<<std::endl;
+		std::cout<<"Blue: "<<blue<<" Green: "<<green<<" Red: "<<red<<std::endl;
+
+		//Set black to the central pixel and its neighbors
+		for (int y = central_pixel_y - 4; y <= central_pixel_y + 4; y ++) {
+    
+			for (int x = central_pixel_x - 4; x <= central_pixel_x + 4; x ++) {
         
-        //show image in a window
-        cv::imshow("Output Window", image);
-		
-		//print image dimensions
-		std::cout << "image size is: " << image.rows << "x" << image.cols << std::endl; 
-		
-		//Waits 1 millisecond to check if a key has been pressed. If so, breaks the loop. Otherwise continues.
-        if(cv::waitKey(1) >= 0) break;
-    }   
+				image.at<cv::Vec3b>(y, x) = 0;
+				
+    		}
+		}
+
+		//Show image in a window
+		cv::imshow("Output Window", image);
+
+		//Waits 1 millisecond to check if a key has been pressed. If so, breaks the loop. Otherwise continues
+		if (cv::waitKey(1) >= 0) break;
+
+	}
+
 }
